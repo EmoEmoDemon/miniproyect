60d4afaab3f70ac33c542b1512cb9170308c873f
EmoEmoDemon <74152410+EmoEmoDemon@users.noreply.github.com>
2023-07-02 12:30:30 -0600

    hola

--- 03-react-issues/src/App.jsx
+++ 03-react-issues/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
 import IssueList from './IssueList';
 
+ReactDOM.render(<IssueList />, document.getElementById('root'));
 const App = () => {
   return (
     <div>
